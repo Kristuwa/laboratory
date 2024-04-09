f60440b8f62359c0b191559ff76c43a5ba77afb6
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2024-04-09 09:53:41 +0300

    fix bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
@@ -13,31 +13,31 @@ const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
   const params = useParams();
 
-  const blocks = {
-    aboutRef: useRef(null),
-    galleryRef: useRef(null),
-    requestRef: useRef(null),
-  };
+  const aboutRef = useRef(null);
+  const galleryRef = useRef(null);
+  const requestRef = useRef(null);
 
-  const scrollToBlock = (block = "") => {
-    if (block in blocks) {
-      blocks[block].current.scrollIntoView({
-        behavior: "smooth",
-      });
-    }
-  };
-
-  useEffect(() => {
-    scrollToBlock(params.block);
-  }, [params]);
+  const scrollToBlock = useCallback((block = "") => {
+	const blocks = { aboutRef, galleryRef, requestRef };
+	if (block in blocks) {
+	  blocks[block].current.scrollIntoView({
+		 behavior: "smooth",
+	  });
+	}
+ }, []);
+ 
+ useEffect(() => {
+	scrollToBlock(params.block);
+ }, [params, scrollToBlock]);
+ 
   return (
     <>
       <Header />
       <main>
         <Hero />
-        <About ref={blocks.aboutRef} />
-        <Gallery ref={blocks.galleryRef} />
-        <Request ref={blocks.requestRef} setIsOpenModal={setIsOpen} />
+        <About ref={aboutRef} />
+        <Gallery ref={galleryRef} />
+        <Request ref={requestRef} setIsOpenModal={setIsOpen} />
       </main>
       <Footer />
       {isOpen && <Modal setIsOpen={setIsOpen} />}
